bf596172d0ca72858bdbf412cd88ae9d543f9b55
MatiasOrzusa <matiasorzu@gmail.com>
2024-06-23 14:14:27 -0400

    Botón editar funcional

--- gestion_personas/update.php
+++ gestion_personas/update.php
@@ -1,30 +1,44 @@
 <?php
 $servername = "localhost";
-$username = "tu_usuario";
-$password = "tu_contraseña";
+$username = "root";
+$password = "";
 $dbname = "gestion_personas";
 
+//conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-//Lee los datos enviados desde el cliente
-$data = json_decode(file_get_contents("php://input"));
 
-$id = $data->id;
-$doce_nombre = $data->doce_nombre;
-$doce_apellido = $data->doce_apellido;
-$per_cumple = $data->per_cumple;
-$per_mail = $data->per_mail;
-$doce_cel = $data->doce_cel;
+//recibe los datos enviados desde el cliente (en formato JSON)
+$data = json_decode(file_get_contents("php://input"), true);
 
-$sql = "UPDATE personas SET doce_nombre='$doce_nombre', doce_apellido='$doce_apellido', per_cumple='$per_cumple', per_mail='$per_mail', doce_cel='$doce_cel' WHERE id=$id";
-//ejecuta la consulta SQL y verifica si se realizo correctamente
-if ($conn->query($sql) === TRUE) {
-    echo json_encode(array("mensaje" => "Registro actualizado exitosamente"));
+//verifica si se recibieron datos válidos
+if (!empty($data['id'])) {
+    //prepara la consulta SQL para actualizar los datos
+    $id = intval($data['id']);
+    $doce_nombre = $conn->real_escape_string($data['doce_nombre']);
+    $doce_apellido = $conn->real_escape_string($data['doce_apellido']);
+    $per_cumple = $conn->real_escape_string($data['per_cumple']);
+    $per_mail = $conn->real_escape_string($data['per_mail']);
+    $doce_cel = $conn->real_escape_string($data['doce_cel']);
+
+    $sql = "UPDATE personas SET doce_nombre='$doce_nombre', doce_apellido='$doce_apellido', per_cumple='$per_cumple', per_mail='$per_mail', doce_cel='$doce_cel' WHERE id=$id";
+
+    if ($conn->query($sql) === TRUE) {
+        //si la actualización fue exitosa, devuelve un mensaje JSON de éxito
+        $response = array("mensaje" => "Datos actualizados correctamente.");
+        echo json_encode($response);
+    } else {
+        //si hubo un error en la consulta SQL, devuelve un mensaje de error
+        $response = array("error" => "Error al actualizar los datos: " . $conn->error);
+        echo json_encode($response);
+    }
 } else {
-    echo json_encode(array("mensaje" => "Error: " . $sql . "<br>" . $conn->error));
+    //si no se recibieron datos válidos, devuelve un mensaje de error
+    $response = array("error" => "Datos recibidos no válidos para actualizar.");
+    echo json_encode($response);
 }
 
 $conn->close();
