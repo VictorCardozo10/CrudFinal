56b2cb257a78c65c9f9cc5a15c83f43a28f640a7
JoshotR18 <137571571+JoshotR18@users.noreply.github.com>
2024-06-26 15:17:13 -0400

    mod en read

--- gestion_personas/read.php
+++ gestion_personas/read.php
@@ -1,34 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gestion_personas";
+$conn = new mysqli("localhost", "root", "", "gestion_personas"); // Crear conexión
+if ($conn->connect_error) die("Conexión fallida: " . $conn->connect_error); // Verificar conexión
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0; // Obtener el id de la URL y convertirlo a entero
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-//Esto lee el parametro "id" desde la URL "($_GET['id'])" y lo convierte a un entero usando "intval()"
-//para evitar inyecciones SQL (insano)
-$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+$sql = $id > 0 ? "SELECT * FROM personas WHERE id = $id" : "SELECT * FROM personas"; // Definir la consulta SQL
+$result = $conn->query($sql); // Ejecutar la consulta y lo guarda en result
 
-if ($id > 0) {
-//realiza una consulta SQL para seleccionar la persona con ese id si es mayor a 0
-    $sql = "SELECT * FROM personas WHERE id = $id";
-    $result = $conn->query($sql);
-    $row = $result->fetch_assoc();
-    echo json_encode($row);
-} else {
-//si no se proporciona un id realiza una consulta SQL para seleccionar todas las personas de una
-    $sql = "SELECT * FROM personas";
-    $result = $conn->query($sql);
-    $personas = array();
-    while ($row = $result->fetch_assoc()) {
-        $personas[] = $row;
-    }
-    echo json_encode($personas);
-}
+$data = $id > 0 ? $result->fetch_assoc() : $result->fetch_all(MYSQLI_ASSOC); // Obtener los resultados
+echo json_encode($data); // Convertir los resultados a JSON y mostrarlos
 
-$conn->close();
+$conn->close(); // Cerrar conexión
 ?>
